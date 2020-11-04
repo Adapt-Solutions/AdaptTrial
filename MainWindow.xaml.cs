b7dfc6faa1ac983a4e084110a71fe2e78594c963
Laura Bailey <laurab@adaptsolutions.com.au>
2020-11-04 14:59:12 +1100

    Added function select + output screen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdaptTrial
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button) sender;
+            var buttonDataContext = (string) button.DataContext;
+            var method = typeof(MainWindow).GetMethod(buttonDataContext) ?? throw new Exception($"Couldn't find the method with the name '{buttonDataContext}'.");
+            method.Invoke(this, null);
+        }
+
+        public void Apple()
+        {
+            var sequence = new List<int> {0, 1, 1, 2, 3, 5, 8, 13};
+            var last = 7;
+
+            for (var i = 0; i < 10; i++)
+            {
+                sequence.Add(sequence[1 - last] + sequence[last]);
+                last++;
+            }
+
+            MenuModel.OutputText = $"The first {sequence.Count} numbers of the Fibonacci sequence are:" + string.Join(", ", sequence.Select(x => x.ToString()));
+        }
+
+        public static void Banana()
+        {
+        }
+
+        public static void Pear()
+        {
         }
     }
 }
---
+++ MenuModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using AdaptTrial.Annotations;
+
+namespace AdaptTrial
+{
+    public sealed class MenuModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _pageOptions = new ObservableCollection<string>();
+        private string _outputText;
+
+        public ObservableCollection<string> PageOptions
+        {
+            get => _pageOptions;
+            set
+            {
+                if (Equals(value, _pageOptions)) return;
+                _pageOptions = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OutputText
+        {
+            get => _outputText;
+            set
+            {
+                if (value == _outputText) return;
+                _outputText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
